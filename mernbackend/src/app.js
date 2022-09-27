cfe341e610f403b851a6b057e2ed2b84a578b960
Rahul <rahul10kr7@gmail.com>
2022-09-27 16:51:56 +0530

    views done

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 require('./db/connection')
 const path = require('path')
-
+const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 8080
-// console.log(path.join(__dirname,"../public"))
+
+
 const staticPath= path.join(__dirname,"../public")
+const templatePath = path.join(__dirname,"../templates/views")
+const partials_path = path.join(__dirname,"../templates/partials")
+
 app.use(express.static(staticPath))
 
 
+app.set('view engine', 'hbs')
+app.set("views", templatePath);
+hbs.registerPartials(partials_path)
+
+
 app.get("/",(req, res) => {
-    res.send("hello from registration page home")
+    res.render("index")
 })
 
 app.listen(port, () => {
